86cc745b2105dc75174f3f2f73c5dd42cf352522
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-08-04 03:33:55 +0530

    admin user controller create

---
+++ app/Http/Controllers/AdminUserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminUserController extends Controller
+{
+    public function showUserList()
+    {
+        return view('admin.user_list');
+    }
+
+    public function showAddUser()
+    {
+        return view('admin.add_user');
+    }
+    public function AddUser(Request $request) {}
+    public function showEditUser()
+    {
+        return view('admin.edit_user');
+    }
+    public function editUser(Request $request) {}
+    public function userDelete(Request $request, $id) {}
+}
