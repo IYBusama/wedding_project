0ca0c94149846376c445a86891ece85de20bcf5b
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-08-04 03:33:44 +0530

    admin event controller create

---
+++ app/Http/Controllers/AdminEventController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminEventController extends Controller
+{
+    public function showAddEvent()
+    {
+        return view('admin.add_event');
+    }
+    public function addEvent(Request $request) {}
+    public function showEditEvent()
+    {
+        return view('admin.edit_event');
+    }
+    public function editEvent(Request $request){
+
+    }
+    public function eventDelete(Request $request,$id){
+        
+    }
+}
