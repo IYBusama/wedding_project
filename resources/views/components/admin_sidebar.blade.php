e874dcecd66d4d655f767b4e9b3dec715bc85424
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-08-04 11:16:23 +0530

    sidebar url update

--- resources/views/components/admin_sidebar.blade.php
+++ resources/views/components/admin_sidebar.blade.php
@@ -9,7 +9,7 @@
                  </a>
              </li>
              <li>
-                 <a href="#" class="flex items-center p-3 rounded-lg hover:bg-gray-100 text-gray-600 transition-colors duration-200">
+                 <a href="/admin/event-list" class="flex items-center p-3 rounded-lg hover:bg-gray-100 text-gray-600 transition-colors duration-200">
                      Events
                  </a>
              </li>
